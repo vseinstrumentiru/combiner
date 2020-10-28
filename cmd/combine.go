c610f53368256a7632b12ecbaafe5aa27d46bc3d
shtrikulis <ilya.shtrikul@vseinstrumenti.ru>
2020-10-28 14:08:35 +0300

    added fast read from combine.yaml

--- cmd/combine.go
+++ cmd/combine.go
@@ -8,63 +8,63 @@ import (
 	"github.com/spf13/viper"
 )
 
-func Combine() *cobra.Command {
-	cfg := combineArgs{groups: map[string][]string{}}
+func CombineCmd() *cobra.Command {
+	cfg := CombineArgs{Groups: map[string][]string{}}
 	cmd := &cobra.Command{
 		Use:   "combine group1:file1,file2 file3 group3:file4,file5 ...",
 		Short: "Combines several config files into one.",
 		Long: `All files in groups will be merged and overwrite the default config file.
 All merged config writes by sections, where section name is a group name.`,
 		Args: cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
+		RunE: func(_ *cobra.Command, args []string) error {
 			for i, arg := range args {
 				parsed := strings.Split(arg, ":")
 
 				switch len(parsed) {
 				case 1:
-					cfg.groups[parsed[0]] = []string{parsed[0]}
+					cfg.Groups[parsed[0]] = []string{parsed[0]}
 				case 2:
-					cfg.groups[parsed[0]] = strings.Split(parsed[1], ",")
+					cfg.Groups[parsed[0]] = strings.Split(parsed[1], ",")
 				default:
 					return fmt.Errorf("wrong argument %d", i+1)
 				}
 			}
 
-			return combine(cfg)
+			return Combine(cfg)
 		},
 	}
 
-	cmd.Flags().StringVarP(&cfg.path, "path", "p", "./values", "Folder with yaml configs to merge")
-	cmd.Flags().StringVarP(&cfg.out, "out", "o", "values.yaml", "Output file name")
+	cmd.Flags().StringVarP(&cfg.Path, "path", "p", "./values", "Folder with yaml configs to merge")
+	cmd.Flags().StringVarP(&cfg.Out, "out", "o", "values.yaml", "Output file name")
 	cmd.Flags().StringVarP(
-		&cfg.baseName,
+		&cfg.BaseName,
 		"default",
 		"d",
 		"default",
-		"Default config file name (without extension). This is base config file for other groups",
+		"Default config file name (without extension). This is base config file for other Groups",
 	)
-	cmd.Flags().BoolVarP(&cfg.withoutBase, "no-default", "n", false, "Without default file config")
+	cmd.Flags().BoolVarP(&cfg.WithoutBase, "no-default", "n", false, "Without default file config")
 
 	return cmd
 }
 
-type combineArgs struct {
-	path        string
-	out         string
-	withoutBase bool
-	baseName    string
-	groups      map[string][]string
+type CombineArgs struct {
+	Path        string
+	Out         string
+	WithoutBase bool
+	BaseName    string
+	Groups      map[string][]string
 }
 
-func combine(args combineArgs) error {
+func Combine(args CombineArgs) error {
 	writer := viper.New()
 
-	for key, group := range args.groups {
+	for key, group := range args.Groups {
 		reader := viper.New()
-		reader.AddConfigPath(args.path)
+		reader.AddConfigPath(args.Path)
 
-		if !args.withoutBase {
-			reader.SetConfigName(args.baseName)
+		if !args.WithoutBase {
+			reader.SetConfigName(args.BaseName)
 			if err := reader.MergeInConfig(); err != nil {
 				return err
 			}
@@ -81,10 +81,10 @@ func combine(args combineArgs) error {
 		}
 	}
 
-	err := writer.WriteConfigAs(args.out)
+	err := writer.WriteConfigAs(args.Out)
 
 	if err == nil {
-		fmt.Printf("successful combined to %s\n", args.out)
+		fmt.Printf("successful combined to %s\n", args.Out)
 	}
 
 	return err
--- main.go
+++ main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/vseinstrumentiru/combiner/cmd"
 )
 
 func main() {
-	root := &cobra.Command{}
+	root := &cobra.Command{
+		RunE: func(_ *cobra.Command, _ []string) error {
+			v := viper.New()
+			v.AddConfigPath(".")
+			v.SetConfigName("combine")
+			v.SetDefault("path", "./values")
+			v.SetDefault("out", "values.yaml")
+			v.SetDefault("baseName", "default")
+			v.SetDefault("withoutBase", false)
+			if err := v.MergeInConfig(); err != nil {
+				return err
+			}
+
+			var cfg cmd.CombineArgs
+			if err := v.Unmarshal(&cfg); err != nil {
+				return err
+			}
+
+			return cmd.Combine(cfg)
+		},
+	}
 
 	root.AddCommand(
-		cmd.Combine(),
+		cmd.CombineCmd(),
 	)
 
 	if err := root.Execute(); err != nil {
